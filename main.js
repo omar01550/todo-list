4c5ea22c07c8a43efae9547c4da24e8cfc7e0586
omar medhat <107444038+omar01550@users.noreply.github.com>
2022-12-30 02:45:41 +0700

    Update main.js

--- main.js
+++ main.js
@@ -1,22 +1,21 @@
-//function create tasks
-//functio add task to array
-//functio add arr to local sotorage;
-//function add tasks to page;
 
-//fucntion ceck tasks
-//function deklete tasks
-//function update task
 
-//global vars
 let form=document.querySelector("form");
 let input=document.querySelector("form input");
 let tasksDiv=document.querySelector(".tasks");
+
+
 let arr=[];
 
+
 if(localStorage.tasksTodo != "undefined" && localStorage.tasksTodo != undefined){
-  arr=JSON.parse(localStorage.tasksTodo)
+  arr=JSON.parse(localStorage.tasksTodo);
 }
-addTasksToPage(arr)
+
+addTasksToPage(arr);
+addDataToCompletedSection()
+
+
 form.onsubmit=function(e){
   e.preventDefault();
   if(input.value != ""){
@@ -25,93 +24,13 @@ form.onsubmit=function(e){
     addTaskToArray(task,arr);
     addArrayToLocalStorage(arr);
     addTasksToPage(arr);
+    addDataToCompletedSection();
     input.value="";
 
   }
 }
 //functions
-function createTask(value){
-   return {
-     content:value,
-     completed:false,
-     id:Math.random()*8100001010
-   }
-}
 
-
-function addTaskToArray(task,arr){
-   arr.push(task);
-}
-function addArrayToLocalStorage(arr){
-  localStorage.setItem("tasksTodo",JSON.stringify(arr));
-}
-function addTasksToPage(arr){
-  tasksDiv.innerHTML="";
-   for(let i=0;i<arr.length;i++){
-     let divTask=`
-     <div class="task" data-check=${arr[0].completed} id=${arr[i].id} data-completed=${arr[i].completed}>
-         <div class="content">
-             <div class="check" style=${arr[i].completed == true?"background-image:linear-gradient(60deg,#673ab7,#9e9e9ef2);":"background-image:transparent;"}>
-
-             </div>
-             <p class="text">${arr[i].content}</p>
-         </div>
-
-         <div class="icons">
-            <i class="fa fa-pen"></i>
-            <i class="fa fa-trash"></i>
-
-         </div>
-
-         <div class="links">
-             <span class="tasks-count"></span>
-             <div class="controls">
-                 <a href="#" class="all">all</a>
-                 <a href="#" class="active-page active">active</a>
-                 <a href="#" class="completed">completed</a>
-             </div>
-             <span class="clear-all">clear all</span>
-         </div>
-
-     </div>
-     `
-     //count tasks
-     tasksDiv.innerHTML+=divTask;
-
-   }
-}
-function deleteTaks(){
-  document.documentElement.addEventListener("click",function(e){
-     if(e.target.classList.contains("fa-trash")){
-        let id=e.target.parentNode.parentNode.id;
-
-        for(let i=0;i<arr.length;i++){
-           if(arr[i].id == id){
-              arr.splice(i,1);
-              addArrayToLocalStorage();
-              addTasksToPage(arr);
-           }
-        }
-     }
-  })
-}
-function updateTask(){
-  document.documentElement.addEventListener("click",function(e){
-     input.focus();
-     if(e.target.classList.contains("fa-pen")){
-        let id=e.target.parentNode.parentNode.id;
-        for(let i=0;i<arr.length;i++){
-           if(arr[i].id == id){
-              input.value=arr[i].content;
-              arr.splice(i);
-              addArrayToLocalStorage()
-              break;
-           }
-        }
-     }
-  });
-
-}
 updateTask();
 deleteTaks();
 //change mood
@@ -120,18 +39,20 @@ mood.onclick=function(){
     changeMood();
     mood.classList.toggle("fa-moon");
     mood.classList.toggle("fa-sun");
-}
-function changeMood(){
-   if(mood.classList.contains("fa-sun")){
-      document.documentElement.style.setProperty("--seconary-color","#f6f6f6");
-      document.documentElement.style.setProperty("--text-color","black");
-      document.documentElement.style.setProperty("--task-color","white");
-   }else{
-     document.documentElement.style.setProperty("--seconary-color","#181824");
-     document.documentElement.style.setProperty("--text-color","white");
-     document.documentElement.style.setProperty("--task-color","#25273c");
-   }
-}
+    let check=document.querySelectorAll(".check");
+
+
+       check.forEach((check, i) => {
+           if(mood.classList.contains("fa-moon")){
+               check.style.border="2px solid black";
+           }else{
+             check.style.border="2px solid white";
+           }
+       });
+
+    }
+
+
 //clear all
 document.documentElement.addEventListener("click",function(e){
     if(e.target.classList.contains("clear-all")){
@@ -193,22 +114,203 @@ document.documentElement.addEventListener("click",function(e){
 
 //check
 
-document.documentElement.addEventListener("click",(e)=>{
-    if(e.target.classList.contains("check")) {
-       let id=e.target.parentNode.parentNode.id;
+document.documentElement.addEventListener("click",function (e) {
+    if (e.target.classList.contains("check-task")) {
+      let id =e.target.parentNode.parentNode.id;
+       let state = e.target.parentNode.parentNode.dataset.completed;
+
        for(let i=0;i<arr.length;i++){
-          if(arr[i].id ==id){
-             if(arr[i].completed == true){
-                arr[i].completed=false;
-                addTasksToPage(arr);
-                addArrayToLocalStorage(arr)
-             }else{
-               arr[i].completed=true;
-               addTasksToPage(arr);
-               addArrayToLocalStorage(arr);
-             }
+          if(arr[i].id == id){
+             arr[i].completed=true;
+
+             addTasksToPage(arr);
+             addDataToCompletedSection();
+             addArrayToLocalStorage(arr);
           }
        }
-
     }
 })
+
+
+
+
+
+
+
+
+//checkIcon
+let activeBtn = document.querySelector(".controls .active-page");
+let completedBtn = document.querySelector(".completed-page");
+let sections = document.querySelectorAll(".section")
+activeBtn.addEventListener("click",function () {
+
+     handelLinks(this);
+})
+
+completedBtn.addEventListener("click",function(){
+    handelLinks(this);
+})
+
+function handelLinks(target) {
+   sections.forEach((section, i) => {
+       section.style.display="none";
+     });
+
+     document.querySelector(target.dataset.display).style.display="block";
+     document.querySelectorAll(".controls .link").forEach((link, i) => {
+         link.style.color="var(--text-color)";
+
+     });
+
+   target.style.color="var(--active-color)";
+
+
+
+}
+
+
+// functions
+
+//function update task
+function updateTask(){
+  document.documentElement.addEventListener("click",function(e){
+     input.focus();
+     if(e.target.classList.contains("fa-pen")){
+        let id=e.target.parentNode.parentNode.id;
+        for(let i=0;i<arr.length;i++){
+           if(arr[i].id == id){
+              input.value=arr[i].content;
+              arr.splice(i,1);
+              addArrayToLocalStorage(arr)
+              break;
+           }
+        }
+     }
+  });
+
+}
+
+//function deleteTaks
+function deleteTaks(){
+  document.documentElement.addEventListener("click",function(e){
+     if(e.target.classList.contains("fa-trash")){
+        let id=e.target.parentNode.parentNode.id;
+
+        for(let i=0;i<arr.length;i++){
+           if(arr[i].id == id){
+              arr.splice(i,1);
+              addArrayToLocalStorage(arr);
+              addTasksToPage(arr);
+           }
+        }
+     }
+  })
+}
+
+// function add tasks to page
+function addTasksToPage(arr){
+   console.log(arr);
+  let newTasks = arr.filter(ele => ele.completed == false);
+  console.log(newTasks);
+  tasksDiv.innerHTML="";
+   for(let i=0;i<newTasks.length;i++){
+     let divTask=`
+     <div class="task" id=${newTasks[i].id} data-completed=${arr[i].completed}>
+         <div class="content">
+             <div class="check-task">
+
+             </div>
+             <p class="text">${newTasks[i].content}</p>
+         </div>
+
+         <div class="icons">
+            <i class="fa fa-pen"></i>
+            <i class="fa fa-trash"></i>
+
+         </div>
+
+
+
+     </div>
+     `
+     //count tasks
+     tasksDiv.innerHTML+=divTask;
+
+   }
+}
+//***************************************
+// function add completed tasks
+
+function addDataToCompletedSection() {
+   let completedArray = arr.filter(ele => ele.completed == true);
+   document.querySelector(".completed-section").innerHTML='';
+   for(let i=0;i<completedArray.length;i++){
+        document.querySelector(".completed-section").innerHTML+=`
+        <div class="task-complete" id=${completedArray[i].id}>
+            <div class="content">
+
+                <p class="text">${completedArray[i].content}</p>
+            </div>
+
+            <div class="icons">
+               <i class="fa fa-undo return "></i>
+
+
+            </div>
+
+
+
+        </div>
+
+
+          `
+   }
+
+
+
+
+}
+
+// return task
+document.documentElement.addEventListener("click",function (e) {
+       if (e.target.classList.contains("return")) {
+           let id =e.target.parentNode.parentNode.id;
+           for(let i=0;i<arr.length;i++){
+              if (arr[i].id == id) {
+                  arr[i].completed=false;
+                  addTasksToPage(arr);
+                  addDataToCompletedSection();
+                  addArrayToLocalStorage(arr);
+              }
+           }
+       }
+})
+
+// function change mode
+function changeMood(){
+   if(mood.classList.contains("fa-sun")){
+      document.documentElement.style.setProperty("--seconary-color","#f6f6f6");
+      document.documentElement.style.setProperty("--text-color","black");
+      document.documentElement.style.setProperty("--task-color","white");
+   }else{
+     document.documentElement.style.setProperty("--seconary-color","#181824");
+     document.documentElement.style.setProperty("--text-color","white");
+     document.documentElement.style.setProperty("--task-color","#25273c");
+   }
+}
+
+function createTask(value){
+   return {
+     content:value,
+     completed:false,
+     id:Math.random()*8100001010
+   }
+}
+
+
+function addTaskToArray(task,arr){
+   arr.push(task);
+}
+function addArrayToLocalStorage(arr){
+  localStorage.setItem("tasksTodo",JSON.stringify(arr));
+}
